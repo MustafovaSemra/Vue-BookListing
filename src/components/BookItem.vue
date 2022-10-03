9fa234902be8887661419657f8bf4fdc02895e40
MustafovaSemra <mustafovasemra@yahoo.com>
2022-10-03 15:06:30 -0400

    BookItem Component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul>
+        <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'BookItem',
+        props: ['book']
+    }
+</script>
+
+<style></style>
